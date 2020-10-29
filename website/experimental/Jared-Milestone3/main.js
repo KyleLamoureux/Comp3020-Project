4f9006fd7e8df3c72faa98df78a7f8b5041dbb06
Jared Castillo <59758243+jablue-12@users.noreply.github.com>
2020-10-28 23:46:38 -0500

    Summary page layout.
    Summary page pops up when checkout is clicked on the menu page.

---
+++ website/experimental/Jared-Milestone3/main.js
@@ -0,0 +1,34 @@
+'use strict';  
+
+  // When the user clicks the button, open the modal 
+  function proceedCheckout() {
+    let modal = document.getElementById("summary-page");
+    modal.style.display = "block";
+    blurControl();
+    loadSummaryPage();
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  function cancelCheckout() {
+    let modal = document.getElementById("summary-page");
+    // Get the <span> element that closes the modal
+    let span = document.getElementsByClassName("close")[0];
+    modal.style.display = "none";
+    blurControl();
+  }
+
+
+  function blurControl(){
+    var containerElement = document.getElementById("background");
+
+    if (containerElement.className == 'blur'){
+        containerElement.setAttribute('class', null);
+    } else {
+        containerElement.setAttribute('class', 'blur');
+    }
+};
+
+
+function loadSummaryPage(){
+  document.getElementById("content").innerHTML='<object type="text/html" data="index.html" ></object>';
+}
