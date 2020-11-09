de3d72e757572ce7fecfc467ffbe070937361b65
Speuce <mrk5199@gmail.com>
2020-11-09 11:06:43 -0600

    Moved Data definitions to separate files

---
+++ website/resources/js/restaurant-categories.js
@@ -0,0 +1,32 @@
+restaurants_categories = [
+    {
+        name: "Burgers",
+        img: "../resources/images/restaurant-page/categories/burger.jpg",
+        active: true
+    },
+    {
+        name: "Pizza",
+        img: "../resources/images/restaurant-page/categories/pizza.jpg",
+        active: true
+    },
+    {
+        name: "Mexican",
+        img: "../resources/images/restaurant-page/categories/mexican.jpg",
+        active: true
+    },
+    {
+        name: "Sushi",
+        img: "../resources/images/restaurant-page/categories/sushi.jpg",
+        active: true
+    },
+    {
+        name: "Pasta",
+        img: "../resources/images/restaurant-page/categories/pasta.jpg",
+        active: true
+    },
+    {
+        name: "Breakfast",
+        img: "../resources/images/restaurant-page/categories/breakfast.jpg",
+        active: true
+    }
+];
---
+++ website/resources/js/restaurants.js
@@ -0,0 +1,62 @@
+/* Data */
+restaurants = [
+    {
+        name: "Perkins",
+        id: "perkins",
+        img: "../resources/images/restaurant-page/restaurants/perkins/perkins.png",
+        type: ["Breakfast"],
+        foodItems: [
+            {
+                name: "Pancakes",
+                img: "../resources/images/restaurant-page/restaurants/perkins/pancake.jpg",
+                restaurant: "Perkins"
+            },
+            {
+                name: "Sandwiches",
+                img: "../resources/images/restaurant-page/restaurants/perkins/sandwich.jpg",
+                restaurant: "Perkins"
+            },
+            {
+                name: "Omelet",
+                img: "../resources/images/restaurant-page/restaurants/perkins/omelet.jpg",
+                restaurant: "Perkins"
+            }
+        ],
+        distance: 3.8,
+        time: 25,
+        price: 20,
+        popularity: 4,
+        description: "Restaurant description :)",
+        href: "link"
+    },
+    {
+        name: "McDonalds",
+        id: "mcdonalds",
+        img: "../resources/images/restaurant-page/restaurants/mcdonalds/mcdonalds.png",
+        type: ["Burgers"],
+        foodItems: [
+            {
+                name: "Big Mac",
+                img: "../resources/images/restaurant-page/restaurants/mcdonalds/bigmac.jpg",
+                restaurant: "McDonalds"
+            },
+            {
+                name: "Fries",
+                img: "../resources/images/restaurant-page/restaurants/mcdonalds/fries.jpg",
+                restaurant: "McDonalds"
+            },
+            {
+                name: "Ice Cream",
+                img: "../resources/images/restaurant-page/restaurants/mcdonalds/icecream.jpg",
+                restaurant: "McDonalds"
+            }
+        ],
+        distance: 6.3,
+        time: 37,
+        price: 5,
+        popularity: 2,
+        description: "Restaurant description :)",
+        href: "link"
+    },
+
+];
