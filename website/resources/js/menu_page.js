2f7527edac84bbd3689b5ef5608075dd30437210
Jared Castillo <59758243+jablue-12@users.noreply.github.com>
2020-11-26 22:08:56 -0600

    deleted a function by mistake.

--- website/resources/js/menu_page.js
+++ website/resources/js/menu_page.js
@@ -996,3 +996,11 @@ function blurControl(){
       containerElement.setAttribute('class', 'blur');
   }
 }//end blurControl
+
+function onlyNumberKey(evt) { 
+  // Only ASCII charactar in that range allowed 
+  var ASCIICode = (evt.which) ? evt.which : evt.keyCode 
+  if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57)) 
+      return false; 
+  return true; 
+} 
